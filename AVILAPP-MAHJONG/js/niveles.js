0f7ee92aaf049993e63e41bce9d4068e4b97528e
Adrian Avila <adrianavilatorres@gmail.com>
2020-03-25 23:02:47 +0100

    actualizacion

---
+++ AVILAPP-MAHJONG/js/niveles.js
@@ -0,0 +1,12 @@
+//----------------------------SONIDO BOTON-----------------------------------
+
+var reproducir = new Audio();
+reproducir.src = "../music/sonidoBoton.mp3";
+        
+function auudios() {
+    
+    reproducir.play();
+    
+    }
+//----------------------------FIN SONIDO BOTON-----------------------------------
+
