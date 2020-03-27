3786860575c4067f5b9735f4ee9bab55a195d200
sam-OMEN <sam@gmail.com>
2020-03-27 21:08:14 +0100

    Bastante(Creditos y varios)

---
+++ AVILAPP-MAHJONG/js/creditos.js
@@ -0,0 +1,22 @@
+
+    
+    setTimeout(function(){ $("#primer").fadeIn(3000); }, 2000);
+    setTimeout(function(){ $("#primer").fadeOut(3000); }, 6000);
+    setTimeout(function(){ $("#segundo").fadeIn(3000); }, 10000);
+    setTimeout(function(){ $("#segundo").fadeOut(3000); }, 14000);
+    setTimeout(function(){ $("#tercero").fadeIn(3000); }, 18000);
+    setTimeout(function(){ $("#tercero").fadeOut(3000); }, 22000);
+    setTimeout(function(){ $("#cuarto").fadeIn(3000); }, 26000);
+    setTimeout(function(){ $("#cuarto").fadeOut(3000); }, 30000);
+
+
+    setTimeout(function(){ $("#quinto").fadeIn(3000); }, 34000);
+    setTimeout(function(){ $("#quinto").fadeOut(3000); }, 38000);
+    setTimeout(function(){ $("#sexto").fadeIn(3000); }, 42000);
+    setTimeout(function(){ $("#septimo").fadeIn(3000); }, 50000);
+    setTimeout(function(){ $("#octavo").fadeIn(3000); }, 46000);
+
+    
+
+
+
