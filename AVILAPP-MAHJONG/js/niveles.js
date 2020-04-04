2b068d0e8134805b23903421607b74696fe53a8e
sam-OMEN <sam@gmail.com>
2020-04-04 16:20:50 +0200

    problema js

--- AVILAPP-MAHJONG/js/niveles.js
+++ AVILAPP-MAHJONG/js/niveles.js
@@ -10,3 +10,37 @@ function auudios() {
     }
 //----------------------------FIN SONIDO BOTON-----------------------------------
 
+var vari;
+
+function mostrar(x){
+
+    vari = x;
+
+    switch(x){
+        case "facil":
+            alert('facil')
+            document.getElementById('tFacil').style.display="flex"
+            document.getElementById('tMedio').style.display="none"
+            document.getElementById('tDificil').style.display="none"
+            document.getElementById('tNideCoña').style.display="none"
+            break;
+        case "medio":
+              alert('medio')
+              document.getElementById('tFacil').style.display="none"
+            document.getElementById('tMedio').style.display="flex"
+            document.getElementById('tDificil').style.display="none"
+            document.getElementById('tNideCoña').style.display="none"
+         break;
+        case "avanzado":
+              alert('avanzado')
+              document.getElementById('tFacil').style.display="flex"
+            document.getElementById('tMedio').style.display="none"
+            document.getElementById('tDificil').style.display="none"
+            document.getElementById('tNideCoña').style.display="none"
+         break;
+    }
+}
+function ventana(){
+    
+}
+
